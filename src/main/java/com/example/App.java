d0f44c71d82826b83a638fa01b091022dc5bc0b6
genzoo <genzoo.kontakt@gmail.com>
2024-04-19 16:33:20 +0200

    configuration changed

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,11 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootConfiguration
+@SpringBootApplication
 @RestController
 public class App 
 {
